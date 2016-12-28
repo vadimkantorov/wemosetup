9dbe017a433992fec4312df0772d4f2099b1b27d
Vadim Kantorov <vadimkantorov@gmail.com>
2016-12-28 05:03:53 +0100

    Update wemosetup.py

--- wemosetup.py
+++ wemosetup.py
@@ -134,7 +134,7 @@ def connecthomenetwork(ssid, password):
 		network_status = device.soap('WiFiSetup', 'GetNetworkStatus', 'NetworkStatus')
 		close_status = device.soap('WiFiSetup', 'CloseSetup', 'status')
 		if network_status not in ['1', '3'] or close_status != 'success':
-			print 'Device failed to connect to the network %s. Try again.' % ((connect_status, network_status), )
+			print 'Device failed to connect to the network: (%s, %s). Try again.' % (connect_status, network_status)
 			continue
 			
 		print '[ok]'
