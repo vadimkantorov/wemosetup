0c5a8e88851ba2a402ed0227535e03950c1c1fae
Vadim Kantorov <vadim.kantorov@gmail.com>
2017-01-21 01:44:01 +0100

    added ifttt

--- README.md
+++ README.md
@@ -4,41 +4,68 @@ A simple Python script to set up WeMo devices supporting:
  - showing a list of discovered devices (via SSDP)
  - adding new bulbs to WeMo bridges, showing state of paired bulbs
  - toggling state of WeMo switch and WeMo bridge devices
+ - connecting to IFTTT
  - working on Windows too
 
 I have tested it with WeMo Insight and WeMo Bridge.
 
 # Examples
 
-> $ python wemosetup.py discover
+## Discover devices
+```shell
+$ python wemosetup.py discover
 
-```
 Discovering WeMo devices
 
 Discovered:
  - WeMo Bridge (10.22.22.1:49152)
 ```
-<br/>
 
-> $ python wemosetup.py connecthomenetwork --ssid \<mywifinetworkname> --password \<mywifinetworkpassword>
+## Connect to home Wi-Fi
+```shell
+$ python wemosetup.py connecthomenetwork --host 10.22.22.1 --port 49152 --ssid \<mywifinetworkname> --password \<mywifinetworkpassword>
 
-```
-Discovering WeMo devices
 
-Discovered:
- - WeMo Bridge (10.22.22.1:49152)
-
-Connecting discovered devices to network "<mywifinetworkname>"
-
- - WeMo Bridge (10.22.22.1:49152) ... [ok]
+Device WeMo Bridge (10.22.22.1:49152) connceted to network "<mywifinetworkname>"
 
 ```
+
+## Add bulbs
+```shell
+$ python wemosetup.py addenddevices --host 10.22.22.1 --port 49152
+
+Paired bulbs: ['DEADBEEFBULBID']
+```
+
+## List bulbs
+```shell
+$ python wemosetup.py getenddevices --host 10.22.22.1 --port 49152
+
+End devices of WeMo Bridge (192.168.0.25:49154)
+ - 1234MYBULBID, state: off
+```
+
+## Toggle bulbs
+```shell
+$ python wemosetup.py toggle --host 10.22.22.1 --port 49152
+
+WeMo Bridge (192.168.0.25:49154) toggled to: on
+```
+
+## Pair with IFTTT
+```shell
+$ python wemosetup.py ifttt --host 10.22.22.1 --port 49152 --imei 123456789
+
+Navigate to the following address to complete pairing:
+https://ifttt.com/wemo_activate?wemopin=GENERATED_MAGIC_WEMO_PIN&done_url=wemo://status=0
+
+and run the following JavaScript code:
+document.getElementById("WeMoAppMobileData").innerHTML = JSON.stringify({uniqueId:"SOME_ID_1", homeId:"SOME_ID_2", signature:"SOME_ID_3"}); doSubmit(1);
+```
+
 # Dependencies
 - Python 2.7
 - openssl (or openssl.exe) binary discoverable in system $PATH
-
-# TODO
-- support IFTTT
 
 # Credits and references
 1. https://web.archive.org/web/20130429034218/http://www.mgalisa.com/?p=91
--- wemosetup.py
+++ wemosetup.py
@@ -14,7 +14,7 @@ import StringIO
 class SsdpDevice:
 	def __init__(self, setup_xml_url, timeout = 5):
 		setup_xml_response = urllib2.urlopen(setup_xml_url, timeout = timeout).read()
-		self.host_port = tuple(os.path.dirname(setup_xml_url).split('//')[1].split(':'))
+		self.host_port = re.search('//(.+):(\d+)/', setup_xml_url).groups()
 		parsed_xml = xml.dom.minidom.parseString(setup_xml_response)
 		self.friendly_name = parsed_xml.getElementsByTagName('friendlyName')[0].firstChild.data
 		self.udn = parsed_xml.getElementsByTagName('UDN')[0].firstChild.data
@@ -72,16 +72,43 @@ class SsdpDevice:
 	def __str__(self):
 		return '%s (%s:%s)' % ((self.friendly_name,) + self.host_port)
 
+class WemoDevice(SsdpDevice):
+	def __init__(self, host, port):
+		SsdpDevice.__init__(self, 'http://%s:%s/setup.xml' % (host, port))
+	
+	@staticmethod
+	def discover_devices(*args, **kwargs):
+		return [re.search('//(.+):(\d+)/', setup_xml_url) for setup_xml_url in SsdpDevice.discover_devices(service_type = 'urn:Belkin:service:basicevent:1', *args, **kwargs)]
+		
+	def encrypt_wifi_password(self, password, meta_array):
+		keydata = meta_array[0][0:6] + meta_array[1] + meta_array[0][6:12]
+		salt, iv = keydata[0:8], keydata[0:16]
+		assert len(salt) == 8 and len(iv) == 16
+
+		stdout, stderr = subprocess.Popen(['openssl', 'enc', '-aes-128-cbc', '-md', 'md5', '-S', salt.encode('hex'), '-iv', iv.encode('hex'), '-pass', 'pass:' + keydata], stdin = subprocess.PIPE, stdout = subprocess.PIPE).communicate(password)
+		encrypted_password = stdout[16:].encode('base64') # removing 16byte magic and salt prefix inserted by OpenSSL
+		encrypted_password += hex(len(encrypted_password))[2:] + ('0' if len(password) < 16 else '') + hex(len(password))[2:]
+		return encrypted_password
+		
+	def generate_auth_code(self, device_id, private_key):
+		expiration_time = int(time.time()) + 200
+		stdout, stderr = subprocess.Popen(['openssl', 'sha1', '-binary', '-hmac', private_key], stdin = subprocess.PIPE, stdout = subprocess.PIPE).communicate('%s\n\n%s' % (device_id, expiration_time))
+		auth_code = "SDU %s:%s:%s" % (device_id, stdout.encode('base64').strip(), expiration_time)
+		return auth_code
+		
+	def prettify_device_state(self, state):
+		return {1 : 'on', 0 : 'off', None : 'unknown'}[state]
+
 def discover():
 	print ''
 	print 'Discovering WeMo devices'
 	print ''
 	
-	setup_xml_urls = sorted(set(SsdpDevice.discover_devices('urn:Belkin:service:basicevent:1') + map('http://10.22.22.1:{0}/setup.xml'.format, range(49151, 49156))))
+	host_ports = sorted(set(SsdpDevice.discover_devices() + [('10.22.22.1', port) for port in range(49151, 49156)]))
 	discovered_devices = []
-	for setup_xml_url in setup_xml_urls:
+	for host_port in host_ports:
 		try:
-			discovered_devices.append(SsdpDevice(setup_xml_url))
+			discovered_devices.append(WemoDevice(*host_port))
 		except urllib2.URLError:
 			continue
 			
@@ -91,61 +118,43 @@ def discover():
 	print ''
 	return discovered_devices
 
-def connecthomenetwork(ssid, password, timeout = 10):
-	def encrypt_wifi_password(password, meta_array):
-		keydata = meta_array[0][0:6] + meta_array[1] + meta_array[0][6:12]
-		salt, iv = keydata[0:8], keydata[0:16]
-		assert len(salt) == 8 and len(iv) == 16
-
-		stdout, stderr = subprocess.Popen(['openssl', 'enc', '-aes-128-cbc', '-md', 'md5', '-S', salt.encode('hex'), '-iv', iv.encode('hex'), '-pass', 'pass:' + keydata], stdin = subprocess.PIPE, stdout = subprocess.PIPE).communicate(password)
-		encrypted_password = stdout[16:].encode('base64') # removing 16byte magic and salt prefix inserted by OpenSSL
-		encrypted_password += hex(len(encrypted_password))[2:] + ('0' if len(password) < 16 else '') + hex(len(password))[2:]
-		return encrypted_password
-	
-	discovered_devices = discover()
-	
-	print 'Connecting discovered devices to network "%s"' % ssid if discovered_devices else ''
-	for device in discovered_devices:
-		sys.stdout.write(' - %s ... ' % device)
-		aps = [ap for ap in device.soap('WiFiSetup', 'GetApList', 'ApList').split('\n') if ap.startswith(ssid + '|')]
-		if len(aps) == 0:
-			print 'Could not find network "%s". Try again.' % ssid
-			continue
-		elif len(aps) > 1:
-			print 'Discovered %d networks with SSID "%s", using the first available..."' % (len(aps), ssid)
-			
-		channel, auth_mode, encryption_mode = re.match('.+\|(.+)\|.+\|(.+)/(.+),', aps[0]).groups()
-		meta_array = device.soap('metainfo', 'GetMetaInfo', 'MetaInfo').split('|')
-		connect_status = device.soap('WiFiSetup', 'ConnectHomeNetwork', 'PairingStatus', args = {
-			'ssid' : ssid,
-			'auth' : auth_mode, 
-			'password' : encrypt_wifi_password(password, meta_array),
-			'encrypt' : encryption_mode,
-			'channel'  : channel
-		})
+def connecthomenetwork(host, port, ssid, password, timeout = 10):
+	device = WemoDevice(host, port)
+	aps = [ap for ap in device.soap('WiFiSetup', 'GetApList', 'ApList').split('\n') if ap.startswith(ssid + '|')]
+	if len(aps) == 0:
+		print 'Could not find network "%s". Try again.' % ssid
+		return
+	elif len(aps) > 1:
+		print 'Discovered %d networks with SSID "%s", using the first available..."' % (len(aps), ssid)
 		
-		time.sleep(timeout)
-		
-		network_status = device.soap('WiFiSetup', 'GetNetworkStatus', 'NetworkStatus')
-		close_status = device.soap('WiFiSetup', 'CloseSetup', 'status')
-		if network_status not in ['1', '3'] or close_status != 'success':
-			print 'Device failed to connect to the network: (%s, %s). Try again.' % (connect_status, network_status)
-			continue
-			
-		print '[ok]'
-
+	channel, auth_mode, encryption_mode = re.match('.+\|(.+)\|.+\|(.+)/(.+),', aps[0]).groups()
+	meta_array = device.soap('metainfo', 'GetMetaInfo', 'MetaInfo').split('|')
+	connect_status = device.soap('WiFiSetup', 'ConnectHomeNetwork', 'PairingStatus', args = {
+		'ssid' : ssid,
+		'auth' : auth_mode, 
+		'password' : device.encrypt_wifi_password(password, meta_array),
+		'encrypt' : encryption_mode,
+		'channel'  : channel
+	})
+	
+	time.sleep(timeout)
+	
+	network_status = device.soap('WiFiSetup', 'GetNetworkStatus', 'NetworkStatus')
+	close_status = device.soap('WiFiSetup', 'CloseSetup', 'status')
+	print 'Device failed to connect to the network: (%s, %s). Try again.' % (connect_status, network_status) if network_status not in ['1', '3'] or close_status != 'success' else 'Device %s connected to network "%s"' % (device, ssid)
+	
 def getenddevices(device = None, host = None, port = None, list_type = 'PAIRED_LIST'):
-	device = device or SsdpDevice('http://%s:%s/setup.xml' % (host, port))
+	device = device or WemoDevice(host, port)
 	end_devices_decoded = device.soap('bridge', 'GetEndDevices', 'DeviceLists', args = {'DevUDN' : device.udn, 'ReqListType' : list_type}).replace('&lt;', '<').replace('&gt;', '>').replace('&quot;', '"')
 	end_devices = {str(elem.getElementsByTagName('DeviceID')[0].firstChild.data) : {'' : None, '1' : 1, '0' : 0}[elem.getElementsByTagName('CurrentState')[0].firstChild.data.split(',')[0]] for elem in xml.dom.minidom.parseString(end_devices_decoded).getElementsByTagName('DeviceInfo')}
 	if host != None and port != None:
 		print 'End devices of %s' % device
 		for device_id, state in sorted(end_devices.items()):
-			print ' - %s, state: %s' % (device_id, {1 : 'on', 0 : 'off', None : 'unknown'}[state])
+			print ' - %s, state: %s' % (device_id, device.prettify_device_state(state))
 	return end_devices
 		
 def addenddevices(host, port, timeout = 10):
-	device = SsdpDevice('http://%s:%s/setup.xml' % (host, port))
+	device = WemoDevice(host, port)
 	
 	device.soap('bridge', 'OpenNetwork', args = {'DevUDN' : device.udn})
 	time.sleep(timeout)
@@ -158,45 +167,64 @@ def addenddevices(host, port, timeout = 10):
 	print 'Paired bulbs: %s' % sorted(set(scanned_bulb_device_ids) & set(paired_bulb_device_ids))
 	
 def toggle(host, port):
-	device = SsdpDevice('http://%s:%s/setup.xml' % (host, port))
+	device = WemoDevice(host, port)
 	if 'Bridge' in device.friendly_name:
 		bulbs = getenddevices(device, 'PAIRED_LIST')
 		new_binary_state = 1 - int(bulbs.items()[0][1] or 0)
 		device.soap('bridge', 'SetDeviceStatus', args = {'DeviceStatusList' : 
 			''.join(['<?xml version="1.0" encoding="utf-8"?>'] +
-				['<DeviceStatus><IsGroupAction>NO</IsGroupAction><DeviceID available="YES">%s</DeviceID><CapabilityID>%s</CapabilityID><CapabilityValue>%s</CapabilityValue></DeviceStatus>'.replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;') % (bulb_device_id, 10006, new_binary_state) for bulb_device_id in bulbs.keys()]
-			)
+				['''<DeviceStatus><IsGroupAction>NO</IsGroupAction><DeviceID available="YES">%s</DeviceID><CapabilityID>%s</CapabilityID><CapabilityValue>%s</CapabilityValue></DeviceStatus>''' % (bulb_device_id, 10006, new_binary_state) for bulb_device_id in bulbs.keys()]
+			).replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;')
 		})
 	else:
 		new_binary_state = 1 - int(device.soap('basicevent', 'GetBinaryState', 'BinaryState'))
 		device.soap('basicevent', 'SetBinaryState', args = {'BinaryState' : new_binary_state})
 	
-	print '%s toggled to: %s' % (device, new_binary_state)
+	print '%s toggled to: %s' % (device, device.prettify_device_state(new_binary_state))
 
-def ifttt(host, port):
-	device = SsdpDevice('http://%s:%s/setup.xml' % (host, port))
-	home_id = ''
-	pin_url = 'https://fw.xbcs.net/apis/http/plugin/generatePin/%s/IFTTT' % home_id
+def ifttt(host, port, device_id):
+	device = WemoDevice(host, port)
+	parse_xml = lambda resp, fields: [doc.getElementsByTagName(field)[0].firstChild.data for doc in [xml.dom.minidom.parseString(resp)] for field in fields]
+	error = lambda status: '%s failed to enable IFTTT: status code %s' % (device, status)
+	
+	home_id, private_key, remote_access_status = parse_xml(device.soap('remoteaccess', 'RemoteAccess', args = {'DeviceId' : device_id, 'DeviceName' : device_id, 'dst' : 0, 'HomeId' : '', 'MacAddr' : '', 'pluginprivateKey' : '', 'smartprivateKey' : '', 'smartUniqueId' : '', 'numSmartDev' : ''}), ['homeId', 'smartprivateKey', 'statusCode'])
+	if remote_access_status != 'S':
+		print error(remote_access_status)
+		return
+		
+	auth_code = device.generate_auth_code(device_id, private_key)
+	activation_code, generate_pin_status = parse_xml(urllib2.urlopen(urllib2.Request('https://api.xbcs.net:8443/apis/http/plugin/generatePin/%s/IFTTT' % home_id, headers = {'Content-Type' : 'application/xml', 'Authorization' : auth_code})).read(), ['activationCode', 'status'])
+	if generate_pin_status != '0':
+		print error(generate_pin_status)
+		return
+	
+	print 'Navigate to the following address to complete pairing:'
+	print 'https://ifttt.com/wemo_activate?wemopin=%s&done_url=wemo://status=0' % activation_code
+	print ''
+	print 'and run the following JavaScript code:'
+	print 'document.getElementById("WeMoAppMobileData").innerHTML = JSON.stringify({uniqueId:"%s", homeId:"%s", signature:"%s"}); doSubmit(1);' % (device_id, home_id, auth_code)
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	subparsers = parser.add_subparsers()
-	
-	subparsers.add_parser('discover').set_defaults(func = discover)
-	
-	cmd = subparsers.add_parser('connecthomenetwork')
-	cmd.add_argument('--ssid', required = True)
-	cmd.add_argument('--password', required = True)
-	cmd.set_defaults(func = connecthomenetwork)
-	
 	common = argparse.ArgumentParser(add_help = False)
 	common.add_argument('--ip', required = True, dest = 'host')
 	common.add_argument('--port', required = True, type = int)
 	
+	parser = argparse.ArgumentParser()
+	subparsers = parser.add_subparsers()
+	
+	subparsers.add_parser('discover').set_defaults(func = discover)
 	subparsers.add_parser('getenddevices', parents = [common]).set_defaults(func = getenddevices)
 	subparsers.add_parser('addenddevices', parents = [common]).set_defaults(func = addenddevices)
 	subparsers.add_parser('toggle', parents = [common]).set_defaults(func = toggle)
-	subparsers.add_parser('ifttt', parents = [common]).set_defaults(func = toggle)
+	
+	cmd = subparsers.add_parser('connecthomenetwork', parents = [common])
+	cmd.add_argument('--ssid', required = True)
+	cmd.add_argument('--password', required = True)
+	cmd.set_defaults(func = connecthomenetwork)
+	
+	cmd = subparsers.add_parser('ifttt', parents = [common])
+	cmd.add_argument('--imei', required = True, type = int, dest = 'device_id')
+	cmd.set_defaults(func = ifttt)
 		
 	args = vars(parser.parse_args())
 	cmd = args.pop('func')
